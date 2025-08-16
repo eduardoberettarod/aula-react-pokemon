0f6b9d9af62d08f0cf711ca491eddf0c74621ec6
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-08-16 14:14:29 -0300

    feat
    criando responsividade e adicionando imagens de energia pokemon nos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,79 @@
 import './App.css'
 import Card from './Card'
 import SomFundo from './SomFundo'
+import Planta from '/img/energia-planta.png'
+import Fogo from '/img/energia-fogo.png'
+import Agua from '/img/energia-agua.png'
 
 function App() {
   const item1 = {
     nome: 'Bulbasaur',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
     numero: '#1',
-    tipo: 'Planta e Venenoso'
+    tipo: 'Planta e Venenoso',
+    energia: Planta
   }
 
   const item2 = {
     nome: 'Ivysaur',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png',
     numero: '#2',
-    tipo: 'Planta e Venenoso'
+    tipo: 'Planta e Venenoso',
+    energia: Planta
   }
 
   const item3 = {
     nome: 'Venusaur',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png',
     numero: '#3',
-    tipo: 'Planta e Venenoso'
+    tipo: 'Planta e Venenoso',
+    energia: Planta
   }
   const item4 = {
     nome: 'Charmander',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
     numero: '#4',
-    tipo: 'Fogo'
+    tipo: 'Fogo',
+    energia: Fogo
   }
 
   const item5 = {
     nome: 'Charmeleon',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png',
     numero: '#5',
-    tipo: 'Fogo'
+    tipo: 'Fogo',
+    energia: Fogo
   }
 
   const item6 = {
     nome: 'Charizard',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png',
     numero: '#6',
-    tipo: 'Fogo'
+    tipo: 'Fogo',
+    energia: Fogo
   }
   const item7 = {
     nome: 'Squirtle',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
     numero: '#7',
-    tipo: 'Água'
+    tipo: 'Água',
+    energia: Agua
   }
 
   const item8 = {
     nome: 'Wartortle ',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png',
     numero: '#8',
-    tipo: 'Água'
+    tipo: 'Água',
+    energia: Agua
   }
 
   const item9 = {
     nome: 'Blastoise',
     foto: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png',
     numero: '#9',
-    tipo: 'Água'
+    tipo: 'Água',
+    energia: Agua
   }
   const pokemon = [item1, item2, item3, item4, item5, item6, item7, item8, item9]
   return (
--- src/Card.jsx
+++ src/Card.jsx
@@ -5,9 +5,10 @@ function Card(props) {
         <>
             <div className='card'>
                 <h2>{props.info.nome}</h2>
-                <img src={props.info.foto} />
+                <img src={props.info.foto} className='pokemon' />
                 <h2>{props.info.numero}</h2>
                 <h2>{props.info.tipo}</h2>
+                <img src={props.info.energia} className='energia-img' />
             </div>
         </>
     )
